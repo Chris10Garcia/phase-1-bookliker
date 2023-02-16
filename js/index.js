c11a3c99d7d68cb31adcf8aa1d75feb5354dda15
Christian Garcia <bones192nd@gmail.com>
2023-02-16 18:43:33 -0500

    pseudo code / function road map

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {});
+
+
+// GET all users -> randomly select one
+
+// GET all data -> pull list of titles -> add list of titles to DOM + eventlistener to each title
+
+// when user clicks title -> GET (single) data for that title -> add book info to DOM + event listener for like /unlike button
+    // -> when button is being built, check if current user is in like list : yes => unlike no => like
+
+// when button is pressed -> get data for user -> invert likes / unlikes -> PATCH data -> update like list on DOM
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+});
